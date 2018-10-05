c2d546f42fed278a91f7f665eec9895a3cb98be8
jonathan Begot <jonat.begot@laposte.net>
2018-10-05 12:09:47 +0200

    algo2 fini

--- algo2.js
+++ algo2.js
@@ -17,18 +17,35 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+
+
+
+var minMax = (array) => {
+  
+  let min = array[0];
+    let max = 0;
+    
+
+  for (let i = 0; i < array.length ; i= i + 1) {
+    
+
+    if (array[i] < min) {
+      min = array[i];
+      
     }
-    if (array[i] = max) {
+    
+    if (array[i] > max) {
       max = array[i];
-    }
-    return array;
   }
-  array.push(min, max);
+ 
 }
+
+let tab = [min,max];
+console.log (tab)
+
+}
+
+
+
+let resu = minMax()
