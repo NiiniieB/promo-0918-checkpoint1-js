af874de85a023c542e3050b16351ce5544462094
Matthieu Petit <matthieuwpetit@gmail.com>
2018-10-05 13:45:33 +0200

    Résolution algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,27 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
-    }
-    if (array[i] = max) {
+let min = 0;
+let max = 0;
+let newArray= [];
+function minMax(array) {
+
+  for (let i= 0; i < array.length; i++) {
+    if (array[i] < array[i-1]) {
+      min = array[i];
+    } 
+    if (array[i] > array[i-1]) {
       max = array[i];
     }
-    return array;
+    
   }
-  array.push(min, max);
+  
+newArray.push(min, max); 
+return newArray
 }
+
+console.log(minMax([-30, 5, 43, 108, -5, -7, 89]));
+
+
+
