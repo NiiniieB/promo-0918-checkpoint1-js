649fbbc13f88983438094fa63d3abd50cc017faf
chelgoui shems <shems.chelgoui@gmail.com>
2018-10-05 12:19:38 +0200

    resolved algo 3

--- algo3.js
+++ algo3.js
@@ -5,5 +5,16 @@
 */
 
 function fibonacci(num) {
-  
+    let varTemp= 0;
+    let var1 = 1;
+    let var2 = 1;
+  for(let i = 1; i<num; i++){
+    varTemp = var1 + var2;
+    var1 = var2;
+    var2 = varTemp;
+  }
+  console.log(varTemp)
 }
+
+
+fibonacci(6);
