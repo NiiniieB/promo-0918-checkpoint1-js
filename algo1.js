91ed07ad727a6e88de573547afde57bb73e286df
Aymeric <ay.neumann@laposte.net>
2018-10-05 11:11:01 +0200

    algo1.js fait

--- algo1.js
+++ algo1.js
@@ -17,3 +17,27 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const nbColonnes = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
+const nbSieges = [];
+const resultat = [];
+
+function createSieges(tab) {
+  for(i = 1; i < 101; i++){
+    tab.push(i);
+  }
+  return tab;
+}
+
+createSieges(nbSieges);
+
+function displayAll(result) {
+  for(i = 0; i < nbColonnes.length; i++) {
+    for(j = 0; j < nbSieges.length; j++){
+      result.push(`${nbColonnes[i]} - ${nbSieges[j]}`);
+    }
+  }
+}
+
+displayAll(resultat);
+console.log(resultat);
