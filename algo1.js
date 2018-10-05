14277030b0095e4f2b1bbf570f627b8ecb20b223
Alexandre <buissonalexandre16@gmail.com>
2018-10-05 09:53:44 +0200

    Ajout resolution algo1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,16 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+function liste(colonne, ligne) {
+  let tab = [];
+  for (let i =1 ; i <= 26 ; i++){
+    for (let j =1; j<= 100; j++){
+      tab.push(i+"-"+j)
+    }
+  }
+
+  return tab;
+}
+
+
+
