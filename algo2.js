2ef27370a34eec5e75f25be48f067b9c37f3853a
olivierbretaud <olivierbretaud@hotmail.fr>
2018-10-05 12:02:41 +0200

    algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,26 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+function minMax(array){
+  let min = array[0];
+  let max = array[0];
+  let result = [];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i] ;
     }
-    if (array[i] = max) {
+    if (array[i] > max) {
       max = array[i];
     }
-    return array;
+    
   }
-  array.push(min, max);
+  result.push(min);
+  result.push(max);
+  return result;
+
 }
+
+let arraytest = [-30, 5, 43, 108, -5, -7, 89];
+
+console.log(minMax(arraytest));
