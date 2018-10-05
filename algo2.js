bfd040783701977d38e546e4565c4dd1b6ccf366
Camille Coutens <camille.coutens@gmail.com>
2018-10-05 13:13:41 +0200

    resolu algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,22 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+const minMax = (array) => {
+  let result = []
+  let max = array[0];
+  let max = array[0];
 
-  for (let i = 1; i = array.length - 1; i+1) {
+  for (let i = 1; i = array.length; i+1) {
     if (array(i) < min) {
-      min = array;
+      min = array[i];
     }
-    if (array[i] = max) {
+    if (array[i] > max) {
       max = array[i];
     }
     return array;
   }
-  array.push(min, max);
+  result.push(min, max);
+  return result
 }
+
+
