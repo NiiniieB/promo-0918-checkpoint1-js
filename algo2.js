97377b918d45a593717215ce8eee8446170cc040
Gaerdy <gaerdy@gmail.com>
2018-10-05 11:50:16 +0200

    Partie 2: Algorithme JS algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,16 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+function minMax(array){
+  let minMaxArr=[array[0], array[0]];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minMaxArr[0]) {
+      minMaxArr[0] = array[i];
     }
-    if (array[i] = max) {
-      max = array[i];
+    if (array[i] > minMaxArr[1]) {
+      minMaxArr[1] = array[i];
     }
-    return array;
   }
-  array.push(min, max);
+  return minMaxArr;
 }
