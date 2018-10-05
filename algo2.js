69a9239e54f07031b3da57634e9e303afde348ec
Monica TARABUSI <code.mta@gmail.com>
2018-10-05 10:38:12 +0200

    algo2.js

---
+++ algo2.js
@@ -0,0 +1,39 @@
+/* 
+* Corriger la fonction minMax()
+* La fonction récupère un tableau de nombres positifs, négatifs ou nuls
+* Et retourne un tableau à 2 entrées contenant la valeur minimale
+* et la valeur maximale du tableau récupéré en entrée
+* 
+* Exemple 1 :
+* Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
+* Résultat : [-65, 67]
+* 
+* Exemple 2 :
+* Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
+* Résultat : [-30, 108]
+* 
+* Exemple 3 :
+* Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
+* Résultat : [7, 85]
+*/
+
+var minMax = (array) => {
+    let min = array[0];
+    let max = array[0];
+    let minmaxArray = [min, max];
+
+    for (let i = 1; i < array.length; i++) {
+    
+        if (array[i] < minmaxArray[0]) {
+        minmaxArray[0] = array[i];
+        }
+        if (array[i] > minmaxArray[1]) {
+        minmaxArray[1] = array[i];
+        } 
+    }
+    console.log(minmaxArray)
+}
+
+const tableau = [4, 6, 35, -65, -9, 0, 67]
+
+minMax(tableau);
