5c47fbaa2a1ef0ef4d6470c77f3af63325f13aa9
alexcanguilhem <alexandre.canguilhem@gmail.com>
2018-10-05 17:52:26 +0200

    checkpoint1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,19 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+function remptab() {
+
+let array = [];
+  for (let i = 1; i < 27; i++) {
+    for (let x = 1; x < 11; x++){
+      array.push(i+'-'+x);
+    }
+    
+  }
+
+
+
+  return array;
+}
+
+console.log(remptab());
