c8fadf28f286b1c926ea4fef32d7c3e3296c9dcb
lydia.dal@live.fr <lydia.dal@live.fr>
2018-10-05 17:27:17 +0200

    algo 2 checkpoint

--- algo2.js
+++ algo2.js
@@ -17,19 +17,21 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+const minMax = (array) => {
+  var min = 100;
+  var max = 0;
 
-  for (let i = 0; i < array.length - 1; i++) {
-    if (array[i] < min) {
-      min = array;
-    }
-    if (array[i] = max) {
-      max = array[i];
-    }
-    return array;
+  for (var i = 0; i < array.length; i++) {
+      if( array[i] < min) {
+          min = array[i]
+      } else if (array[i] > max) {
+          max = array[i];
+      }
   }
-  array.push(min, max);
+  return [min,max];
 }
 
+console.log(minMax([4, 6, 35, -65, -9, 0, 67]));
+console.log(minMax([-30, 5, 43, 108, -5, -7, 89]));
+console.log(minMax([56, 7, 63, 9, 7, 12, 85]));
+ 
