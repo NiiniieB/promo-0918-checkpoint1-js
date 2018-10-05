55d1030ba1ecb61ab80795cf42199d4d22f4afd0
olivier chabot <chabot.oliv@gmail.com>
2018-10-05 09:52:35 +0200

    Algo1.js finish

--- algo1.js
+++ algo1.js
@@ -1,11 +1,11 @@
 /*
 Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
-Dans la salle les sièges sont répartis de la manière suivantes : 
+Dans la salle les sièges sont répartis de la manière suivantes :
   - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
   - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
 
-Au final la liste devra ressembler à : 
-1-1 
+Au final la liste devra ressembler à :
+1-1
 1-2
 1-3
 .
@@ -17,3 +17,17 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const listSeat = () => {
+  let array = [];
+  let cpt = 0;
+    for (let i = 1; i<27; i++) {
+      for (let j = 1; j<101; j++) {
+        array[cpt]=i+"-"+j;
+        cpt++;
+      }
+    }
+  return array;
+}
+
+console.log(listSeat());
