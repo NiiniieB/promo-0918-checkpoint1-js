1838d70d4ab40ac876209c1fb53e48248834eb2b
Julien Chapron <julien.chapron@protonmail.ch>
2018-10-05 11:01:20 +0200

    ajout du dossier bootsrap

--- algo1.js
+++ algo1.js
@@ -16,4 +16,16 @@ Au final la liste devra ressembler à :
 26-100
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
+
 */
+var daysArray = ["1", "2", "3", "4", "5"];
+var courseHwork = ["4", "8", "15", "16", "23", "42"];
+
+var arr = daysArray.concat(courseHwork);
+var sorted_arr = arr.sort();
+var results = [];
+for (var i = 0; i < arr.length - 1; i++) {
+    if (sorted_arr[i + 1] == sorted_arr[i]) {
+        console.log(results.push(sorted_arr[i]));
+    }
+}
