8443fb3dfdaae94af391f27a5cf64cd86cd350b4
olivier chabot <chabot.oliv@gmail.com>
2018-10-05 10:52:22 +0200

    Finish Algo3

--- algo3.js
+++ algo3.js
@@ -5,5 +5,13 @@
 */
 
 function fibonacci(num) {
-  
+  const arrFibo = [0,1]
+  for (let i = 1; i < num+1; i++) {
+    arrFibo.push(arrFibo[i-1]+arrFibo[i]);
+  }
+  return arrFibo[num+1];
 }
+
+// Number to test
+const number = 10;
+console.log(fibonacci(number));
