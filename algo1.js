6d236349fe42016cb5690a2bc7cb85ae4d5cb0a7
PhilippeYu <yu..philippe@gmail.com>
2018-10-05 10:27:31 +0200

    Exercise 1 done

--- algo1.js
+++ algo1.js
@@ -17,3 +17,14 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const numSieges = () => {
+  const col = [];
+  for (let i = 1; i <= 26; i++){
+    for (let j = 1; j <= 100; j++){
+      col.push(i+"-"+j);
+  }
+}
+  return col
+}
+console.log(numSieges())
