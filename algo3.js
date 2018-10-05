5616961722c4fe72a559799a0a53cd5a427c8ff8
Alexandre <buissonalexandre16@gmail.com>
2018-10-05 10:06:39 +0200

    Resolution algo3

--- algo3.js
+++ algo3.js
@@ -3,7 +3,16 @@
 * Ex: fibo(6)  // return 13;
 * Ex: fibo(10) // return 89;
 */
-
-function fibonacci(num) {
+ function fibonacci(num){
+    let a = 1;
+    let b = 0;
+    let x = 0
   
+    for(let i = num; i >= 0; i--){ 
+      x = a;
+      a = a + b;
+      b = x;
+    }
+  
+    return b;
 }
