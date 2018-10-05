b6237af35d92036b5d12bf04a21ed6a24e79406a
PhilippeYu <yu..philippe@gmail.com>
2018-10-05 11:01:28 +0200

    Exercise 3 done

--- algo3.js
+++ algo3.js
@@ -4,6 +4,19 @@
 * Ex: fibo(10) // return 89;
 */
 
-function fibonacci(num) {
-  
+ let fibonacci = (num) => {
+	let x = 1;
+	let y = 0; 
+	let z;
+
+  	while (num >= 0){
+    		z = x;
+    		x = x + y;
+    		y = z;
+    		num--;
+  	}
+
+  	return y;
 }
+
+console.log(fibonacci(10));
