14867a21005e53050a8a9b98c2c8680545079bb8
Axielles <awacks@gmail.com>
2018-10-05 11:47:24 +0200

    algo3

--- algo3.js
+++ algo3.js
@@ -3,7 +3,19 @@
 * Ex: fibo(6)  // return 13;
 * Ex: fibo(10) // return 89;
 */
-
 function fibonacci(num) {
-  
+    let result = 0
+    x = 0
+    y = 1
+    for (i = 0; i < num; i++) {
+        result = x + y
+        x = y
+        y = result
+    }
+return result
 }
+
+let index = 6
+console.log(`Index: ${index} - fibonacci: ${fibonacci(index)}`)
+index = 10
+console.log(`Index: ${index} - fibonacci: ${fibonacci(index)}`)
