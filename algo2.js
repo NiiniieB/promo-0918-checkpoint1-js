d93758ffc631c01354ac349c463ce72f1d0141f1
Vivien POUGET <vivien.pouget@hotmail.fr>
2018-10-05 11:34:32 +0200

    Partie 2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+let minMax = (array) => {
+  let min = 99999;
+  let max = 0;
+  let answer=[];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    if (array[i] = max) {
+    else if (array[i] > max) {
       max = array[i];
     }
-    return array;
   }
-  array.push(min, max);
+  answer.push(min, max);
+  return answer;
+
 }
