bfdf48a4bda5f180c4abff573ffdc5a068c8a704
Florent Perticoz <perticoz@hotmail.com>
2018-10-05 12:25:22 +0200

    algo3

--- algo3.js
+++ algo3.js
@@ -5,5 +5,16 @@
 */
 
 function fibonacci(num) {
+	let x =[1,2];
+	if (x == 1) {return x[0]};
+	if (x == 2) {return x[1]};
+
+	if (num>2){
+		for (i =2; i< num; i ++ ){
+			x[i]= x[i-1]+x[i-2];
+		}
+	}
+	return x[num-1];	
+	}
   
-}
+console.log(fibonacci(111));
