1da3e9906f0f93ea21fd99a02441be73d4c216a1
Barbara <bkpadet@gmail.com>
2018-10-05 12:48:47 +0200

    reponses algo

--- algo1.js
+++ algo1.js
@@ -16,4 +16,22 @@ Au final la liste devra ressembler à :
 26-100
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
+
 */
+
+let colonne = 26;
+let siege = 100;
+const theatre =() => {
+tab = []
+  for (let i=1; i<colonne+1; i++){
+    for (let j=1; j<siege+1; j++){
+      tab.push (i +'-'+j)
+    }
+  }
+  return tab
+}
+console.log(theatre());
+
+
+
+
--- algo2.js
+++ algo2.js
@@ -17,18 +17,11 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
-
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
-    }
-    if (array[i] = max) {
-      max = array[i];
-    }
-    return array;
-  }
-  array.push(min, max);
+array = [56, 7, 63, 9, 7, 12, 85]
+let minMax =() => {
+let tab =[]
+  tab[0] = Math.min(...array)
+  tab[1] = Math.max(...array)
+  return tab
 }
+console.log(minMax(array));
