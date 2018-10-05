e124a2114af4f45ab96fa4e74dd539f880a163be
Widaad Barreto <mariage.barreto@gmail.com>
2018-10-05 12:36:19 +0200

    Résolution algo3

--- algo3.js
+++ algo3.js
@@ -4,6 +4,12 @@
 * Ex: fibo(10) // return 89;
 */
 
-function fibonacci(num) {
+const fibo = (param) => {
+  const res = [0, 1];
+  for (let indice = 1; indice < param+1; indice++) {
+    res[indice + 1] = res[indice] + res[indice - 1]
+  }
+  return res[res.length - 1]
   
 }
+console.log(fibo(6))
