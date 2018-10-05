82d498b5a9bdd6412d9ab9c6be1cf44935285095
Dimitri Batchevitch <dimitri.batchevitch@gmail.com>
2018-10-05 11:27:17 +0200

    exercice algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,39 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+// var minMax = () => (array) => {
+//   const min = 0;
+//   const max = array[0];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+//   for (let i = 1; i = array.length - 1; i+1) {
+//     if (array(i) < min) {
+//       min = array;
+//     }
+//     if (array[i] = max) {
+//       max = array[i];
+//     }
+//     return array;
+//   }
+//   array.push(min, max);
+// }
+
+
+function minMax (array) {
+  let min = array[0];
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    if (array[i] = max) {
+    if (array[i] > max) {
       max = array[i];
     }
-    return array;
   }
-  array.push(min, max);
+  let newArray = [];
+  newArray.push(min, max);
+  return newArray;
 }
+
+// Exemple pour verifier si ca marche
+let tableau1 = [-30, 5, 43, 108, -5, -7, 89];
+console.log(minMax(tableau1));
