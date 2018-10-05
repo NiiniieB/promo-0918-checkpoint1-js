281817d652e3cfc68ed4655caf16889eaf03efdd
Mayeul Lefebvre <mayeul.lefebvre@gmail.com>
2018-10-05 11:01:43 +0200

    Algo3 done

--- algo3.js
+++ algo3.js
@@ -5,5 +5,18 @@
 */
 
 function fibonacci(num) {
-  
+    if (num == 0) {
+        return 0;
+    }
+    if (num == 1) {
+        return 1;
+    }
+    let fiboChar1 = 0;
+    let fiboChar2 = 1;
+    for (let i = 2; i <= num; i++) {
+        fiboChar = fiboChar2 + fiboChar1;
+        fiboChar1 = fiboChar2;
+        fiboChar2 = fiboChar;
+    }
+    return fiboChar;
 }
