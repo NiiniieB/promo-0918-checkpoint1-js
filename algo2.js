d0409883bd8ea48413fb2bf36ee78cc58697b418
olivier chabot <chabot.oliv@gmail.com>
2018-10-05 10:42:17 +0200

    Finish Algo2

--- algo2.js
+++ algo2.js
@@ -1,34 +1,39 @@
-/* 
+/*
 * Corriger la fonction minMax()
 * La fonction récupère un tableau de nombres positifs, négatifs ou nuls
 * Et retourne un tableau à 2 entrées contenant la valeur minimale
 * et la valeur maximale du tableau récupéré en entrée
-* 
+*
 * Exemple 1 :
 * Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
 * Résultat : [-65, 67]
-* 
+*
 * Exemple 2 :
 * Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
 * Résultat : [-30, 108]
-* 
+*
 * Exemple 3 :
 * Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
-
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+const minMax = (array) => {
+  let min = 0;
+  let max = 0;
+  const arrayFinal=[0,0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min=array[i];
+      arrayFinal[0]=min;
+    } else if (array[i] > max) {
+      max=array[i];
+      arrayFinal[1]=max;
     }
-    if (array[i] = max) {
-      max = array[i];
-    }
-    return array;
   }
-  array.push(min, max);
+  return arrayFinal;
 }
+
+const array=[-30, 5, 43, 108, -5, -7, 89];
+console.log(minMax(array));
+
+
