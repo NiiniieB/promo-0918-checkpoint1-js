c29a9c2926d953531df0e8267cfa72ee08d22b1e
Elisa Bourgeois <elisabourgeois.eb@gmail.com>
2018-10-05 09:54:51 +0200

    Resolution algo1.js

--- algo1.js
+++ algo1.js
@@ -17,3 +17,18 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const fDisplaySeats = (iIntRow, iIntCol) => {
+
+  let arr = []
+  for (let i = 1; i <= iIntRow; i++) {
+    for (let j = 1; j <= iIntCol; j++) {
+      arr.push(`${i}-${j}`); 
+      console.log(`${i}-${j}`);  
+    }
+  }
+  return arr;
+
+}
+
+fDisplaySeats(26, 100);
