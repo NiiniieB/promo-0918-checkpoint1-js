4b1b947d1418f0af5e99355d2a2230daf569e572
David Huveau <d.huveau@gmail.com>
2018-10-05 10:02:12 +0200

    algo3 fait

--- algo3.js
+++ algo3.js
@@ -3,7 +3,18 @@
 * Ex: fibo(6)  // return 13;
 * Ex: fibo(10) // return 89;
 */
+let calculFibo = (cpt, num, val1, val2) => {
+    if (cpt < num) {
+        cpt++;
+        return calculFibo(cpt, num, val2, val1 + val2);
+    }
+    else
+        return val2;
+};
 
 function fibonacci(num) {
-  
-}
+    return calculFibo(1, num, 1, 1);
+};
+
+console.log(fibonacci(6));
+console.log(fibonacci(10));
