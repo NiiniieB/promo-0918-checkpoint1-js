8cd04717a2a7a5b2bc61fc7adae5b088e14c13d5
fjulien <fettingerjulien@yahoo.fr>
2018-10-05 13:17:24 +0200

    modification de l'algo1.js

--- algo1.js
+++ algo1.js
@@ -18,9 +18,11 @@ Au final la liste devra ressembler à :
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
 show = () => {
+  result = []
   for (let i=1 ; i<=26 ; i++){
     for (let j=1 ; j<=100 ; j++)
-    console.log (i +'-'+ j)
+     (result.push(i+"-"+  j))
   }
+  console.log(result)
 }
 show()
