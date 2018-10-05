019f086417d37bfd1d1200de6742b8b367ac20e1
chelgoui shems <shems.chelgoui@gmail.com>
2018-10-05 12:04:10 +0200

    resolved algo 2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,9 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+let arr1 = [1,-15,78,8308707,-4452];
+let arr2 = new Array();
+arr2.push(Math.max(...arr1));
+arr2.push(Math.min(...arr1));
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
-    }
-    if (array[i] = max) {
-      max = array[i];
-    }
-    return array;
-  }
-  array.push(min, max);
-}
+console.log(arr2);
