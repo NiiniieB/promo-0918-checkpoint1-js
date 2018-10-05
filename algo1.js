aa35c2e54fff176e4beb6135be63529fa5c62cde
CharlesC51 <c.crepin@net-c.fr>
2018-10-05 12:37:15 +0200

    algo1 done

--- algo1.js
+++ algo1.js
@@ -17,3 +17,19 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const list = []
+  for (let i=1; i<=26; i++){
+      for (let s=1; s<=100; s++){
+        list.push(i + "-" + s)
+    }          
+  }
+
+console.log(list)
+
+// j'ai 1 salle ( tableau ) qui contient 26 colonnes de sièges
+// chaque colonnes de sièges contient 100 sièges
+// je veut que ma liste affiche le numéro de la colonne ainsi que le numéro de siège
+
+// pour que mon tableau affiche chaque siège de chaque colonne 
+// je doit ajouté un élément 1 et rajouté +1 a chaque élément jusqu'a atteindre 100
