28c007756fbf2dd44e65c5bff17d59fee11146d5
RmTrnsc <tournesacromain@gmail.com>
2018-10-05 12:09:54 +0200

    Résolution de l'algorithme numéro 2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,22 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
-
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
-    }
-    if (array[i] = max) {
-      max = array[i];
-    }
-    return array;
+function minMax(array) {
+  let min = 100;
+  let max = 0;
+  let resultArray = [];
+  for (let i=0; i<array.length; i++) {
+  if (array[i] <= min) {
+  min = array[i];
   }
-  array.push(min, max);
-}
+  if (array[i]>=max){
+  max = array[i];
+  }
+  }
+  resultArray.push(min);
+  resultArray.push(max);
+  return resultArray
+  }
+console.log(minMax([4, 6, 35, -65, -9, 0, 67]))
+console.log(minMax([-30, 5, 43, 108, -5, -7, 89]))
+console.log(minMax([56, 7, 63, 9, 7, 12, 85]))
