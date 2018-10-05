ade932626249adc26a87ef753208b9218349f085
Af1ne <delphine.brunet@gmail.com>
2018-10-05 11:57:01 +0200

    add algo3.js solved

--- algo3.js
+++ algo3.js
@@ -5,5 +5,11 @@
 */
 
 function fibonacci(num) {
-  
+    let fibo = [1, 2];
+    let newEntry;
+    for (let i = 0; i < 50; i++) {
+        newEntry = fibo[i] + fibo[i + 1];
+        fibo.push(newEntry);
+    }
+    return fibo[num - 1]
 }
