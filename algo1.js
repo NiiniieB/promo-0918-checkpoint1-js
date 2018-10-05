dcb6a8953d6200294a475941e93f3f6740d59484
Ioannis Nikas <ioa.nikas@gmail.com>
2018-10-05 11:22:23 +0200

    Réponses Algorithmes JS1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,14 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+function nbSieges(){
+  let numerosSieges = [];
+
+  for (let i = 1; i <= 26; i++) {
+    for (let j = 1; j <= 100; j++) {
+      numerosSieges.push(i + '-' + j);
+    }
+  }
+  console.log(numerosSieges);
+}
+nbSieges();
--- algo2.js
+++ algo2.js
@@ -17,18 +17,28 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+const arrayUn = [4, 6, 35, -65, -9, 0, 67];
+const arrayDeux = [-30, 5, 43, 108, -5, -7, 89];
+const arrayTrois = [56, 7, 63, 9, 7, 12, 85];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+function minMax(array) {
+  let min = array[0];
+  let max = array[0];
+  let newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (array[i] <= min) {
+      min = array[i]
     }
-    if (array[i] = max) {
+    else if (array[i] >= max) {
       max = array[i];
     }
-    return array;
   }
-  array.push(min, max);
+  newArray.push(min,max);
+  console.log(newArray);
 }
+
+minMax(arrayUn);
+minMax(arrayDeux);
+minMax(arrayTrois);
