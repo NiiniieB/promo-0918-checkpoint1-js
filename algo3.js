59766c8aab09e3527f3ab2950e060b3b1dc67c30
Robin Guerre <rb.guerre@gmail.com>
2018-10-05 10:15:34 +0200

    ex algo3

--- algo3.js
+++ algo3.js
@@ -4,6 +4,13 @@
 * Ex: fibo(10) // return 89;
 */
 
-function fibonacci(num) {
-  
-}
+const fibonacci = num => {
+  const fibLength = 50;
+  const fibbage = new Array(fibLength);
+  fibbage[0] = 1;
+  fibbage[1] = 1;
+  for (let i = 2; i < fibLength; i++) {
+    fibbage[i] = fibbage[i - 1] + fibbage[i - 2];
+  }
+  return fibbage[num];
+};
