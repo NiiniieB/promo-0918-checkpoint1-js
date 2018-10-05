4848ac1db97345faa299edc035d572028f73dcff
hdk <amir.madi34@gmail.com>
2018-10-05 10:00:55 +0200

    solution algo1.js

---
+++ algo1.js
@@ -0,0 +1,38 @@
+/*
+Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
+Dans la salle les sièges sont répartis de la manière suivantes : 
+  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
+  - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
+
+Au final la liste devra ressembler à : 
+1-1 
+1-2
+1-3
+.
+.
+.
+26-98
+26-99
+26-100
+
+Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
+*/
+
+function total_seats(numberOfColums){
+
+  let seatsPerColumns = 100;
+  let total = [];
+  for(let i=1; i <= numberOfColums; i++){
+    
+    for(var j=1;j <= seatsPerColumns; j++){
+         
+          total += i  + "-" + j + "-";
+    }
+
+  }
+  
+   
+  return total;
+}
+
+console.log(total_seats(26));
