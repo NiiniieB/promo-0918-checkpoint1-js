706ebb28f4eaec7fc2aa5147d467b9b81e91d594
Dimitri Batchevitch <dimitri.batchevitch@gmail.com>
2018-10-05 12:09:18 +0200

    exercice algo3

--- algo3.js
+++ algo3.js
@@ -5,5 +5,13 @@
 */
 
 function fibonacci(num) {
-  
+  let myArray = [0, 1];
+  for (i=2; i<=num+2; i++){
+      myArray.push();
+      myArray[i] = myArray[i-1] + myArray[i-2];
+  }
+  return myArray[num+1];
 }
+
+//Exemple
+console.log(fibonacci(6));
