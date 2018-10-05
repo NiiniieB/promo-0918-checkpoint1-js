51a49b64589ef6194603b3a093c9f983b4a2a7bd
Monica TARABUSI <code.mta@gmail.com>
2018-10-05 14:47:26 +0200

    petit dernier :-)

--- algo3.js
+++ algo3.js
@@ -1,9 +1,17 @@
 /*
-* Given a number N return the index value of the Fibonacci sequence.
+* Given a number N return the i value of the Fibonacci sequence.
 * Ex: fibo(6)  // return 13;
 * Ex: fibo(10) // return 89;
 */
 
-function fibonacci(num) {
-  
-}
+indice = (numero) => {
+    let depart = [0,1];
+    for (let i = 0; i < numero; i++) {
+        depart.push(depart[0]+depart[1]);
+        depart.shift();   
+    }
+    console.log(depart[1]);
+    
+};
+
+indice(6);
