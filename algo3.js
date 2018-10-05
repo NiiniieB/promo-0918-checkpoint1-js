bdc57645d23540ad92b7c93affda3b77682f526b
Elisa <elucchini17@gmail.com>
2018-10-05 12:27:07 +0200

    résolution algo3

--- algo3.js
+++ algo3.js
@@ -4,6 +4,14 @@
 * Ex: fibo(10) // return 89;
 */
 
-function fibonacci(num) {
-  
+function fibonacci(array, a, b, n) {
+  if (n > 0) {
+    n = n-1;
+    let c = a + b;
+    a = b;
+    b = c;
+    array.push(c)
+    return fibonacci(array, a, b, n);
+  }
+  return array[array.length-1]
 }
