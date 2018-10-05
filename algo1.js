eb5b034f94cdf94107a3dc89579cfcce3928cef5
Anaele Salvatore <anaele.salvatore@gmail.com>
2018-10-05 13:09:45 +0200

    algo modifiés mais non aboutis et bootstrap

--- algo1.js
+++ algo1.js
@@ -17,3 +17,11 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const 
+
+
+arraySiege () => (array1) {
+  ligneSieges = 26
+  for (let i = 1, )
+}
--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
+let minMax = () => (array) => {
   const min = 0;
   const max = array[0];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+  for (let i = 1; i < array.length - 1; i++) {
+    if (array[i] < min) {
+      min = min + array[i];
     }
-    if (array[i] = max) {
-      max = array[i];
+    if (array[i] > max) {
+      max = max + array[i];
     }
     return array;
   }
   array.push(min, max);
 }
+
+minMax(4, 6, 35, -65, -9, 0, 67)
