775d609e371859b33a7b6a156096c36f1104ca0b
Valentin Berruer <valb37@icloud.com>
2018-10-05 11:21:14 +0200

    algo2.js

--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
-
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+let minMax = (array) => {
+  let myArray = [];
+  let min = array[0];
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    if (array[i] = max) {
+    if (array[i] > max) {
       max = array[i];
-    }
-    return array;
+    }    
   }
-  array.push(min, max);
+  myArray.push(min, max);
+  return myArray;
 }
+console.log( minMax([4, 6, 35, -65, -9, 0, 67]) );
+
