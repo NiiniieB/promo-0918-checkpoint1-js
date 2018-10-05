2abbbbdd520b0de5ed7698627c6e1e4fbbc165ec
Geoffroy Periot <geoffroy.periot@gmail.com>
2018-10-05 10:56:52 +0200

    gallery + gallery bonus OK

--- algo3.js
+++ algo3.js
@@ -5,5 +5,20 @@
 */
 
 function fibonacci(num) {
-  
+    let fiboNum = 0
+    let fiboArr = [1]
+    for(let i = 1 ; i <= num; i++) {
+        if (i!==1) {
+            fiboNum += fiboArr[i-1]
+        } else {
+            fiboNum += 1;
+        }
+        fiboArr.push(fiboNum)
+    }
+    return fiboArr[num];
 }
+
+console.log(fibonacci(0))
+console.log(fibonacci(1))
+console.log(fibonacci(5))
+console.log(fibonacci(12))
