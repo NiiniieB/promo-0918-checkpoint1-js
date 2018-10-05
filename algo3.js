24e12d2c5174e7ba2729637c0dd85f3bb4cd5182
antoinemta <antoine_mta@hotmail.fr>
2018-10-05 11:53:11 +0200

    I resolved the algo3 .

--- algo3.js
+++ algo3.js
@@ -5,5 +5,20 @@
 */
 
 function fibonacci(num) {
+	
+	let a=[];
+	a[0]=0;
+	let b=[];
+	b[0]=1;
   
+	for (let i=1; i<num; i++)
+	{
+		a[i]=b[i-1]
+		b[i]=a[i-1]+b[i-1]
+	}
+
+
+	return a[num-1]+b[num-1];
 }
+
+console.log(fibonacci(6));
