dd4d253444285472a2d687fed56268cbc658e6a3
Romain Castillon <castillonromain01@gmail.com>
2018-10-05 11:01:52 +0200

    third exercise done

--- algo3.js
+++ algo3.js
@@ -4,6 +4,21 @@
 * Ex: fibo(10) // return 89;
 */
 
-function fibonacci(num) {
-  
+function fibonacci(param) {
+	let x = 1;
+	let y = 0; 
+	let z=0;
+
+  	while (param>= 0){
+    		z = x;
+    		x = x + y;
+    		y = z;
+    		param--;
+  	}
+
+  	return y;
 }
+
+console.log(fibonacci(6));
+  
+
