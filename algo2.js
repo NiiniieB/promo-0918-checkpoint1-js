d070697e6dc852daf2fbd3f70d01ed6c4e2c37ad
Mathieu De Armey <mdearmey@gmail.com>
2018-10-05 12:59:16 +0200

    Algo 2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,23 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+var minMax = (array) => {
+  let min = array[0];
+  let max = array[0];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    if (array[i] = max) {
+    if (array[i] > max) {
       max = array[i];
     }
-    return array;
+      
   }
-  array.push(min, max);
+let result = [min, max]
+return result
+  
 }
+
+console.log(minMax([]))
