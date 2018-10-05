9bf4841b7230331c17d54767bbdb28e2aecdd9b3
compainkevin <kevincompain1@gmail.com>
2018-10-05 11:50:11 +0200

    algo1.js Fait

--- algo1.js
+++ algo1.js
@@ -16,4 +16,33 @@ Au final la liste devra ressembler à :
 26-100
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
+
+Créer une fonction list sans paramètre.
+Déclarer dans cette fonction deux variables : -let colonnes
+					      -let sièges
+Créer une boucle for qui permet d'incrémenter 100 sièges dans chaque colonne soit 26 fois.
+
+Afficher cette boucle de façon : 1-1
+				 1-...
+				 1-100
+				 .....
+				 26-99
+				 26-100
 */
+
+function list () {
+	var sieges;
+	var colonnes;
+
+for ((colonnes = 1; colonnes <= 26; colonnes ++)
+	
+	for (sieges = 1; sieges <= 100; sieges ++))
+
+return
+
+}
+
+console.log("colonnes-sieges")
+
+	
+
