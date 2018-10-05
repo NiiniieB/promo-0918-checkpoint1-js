0657c01ff80f42076ab00c01a5e9760365af0755
P-Hugo <ph.meillot@gmail.com>
2018-10-05 13:00:40 +0200

    alog3 check

--- algo3.js
+++ algo3.js
@@ -4,6 +4,10 @@
 * Ex: fibo(10) // return 89;
 */
 
-function fibonacci(num) {
-  
+function fibonacci(N) {
+    let result = [1, 1];
+    for (let i = 2; i < N + 1; i++) {
+        result[i] = result[i - 1] + result[i - 2];
+    }
+    return result[N];
 }
