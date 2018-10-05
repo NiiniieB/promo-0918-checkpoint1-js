ad9fd2131d3cb128098763a9744c9737e6809ce3
Gaerdy <gaerdy@gmail.com>
2018-10-05 11:25:17 +0200

    Partie 2: Algorithme JS algo1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,17 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+function seatList(){
+  let seatArray=[];
+  let counter=0;
+  for (let i=0; i<26; i++){
+    for (let j=0; j<100; j++){
+      seatArray[counter]=(i+1)+"-"+(j+1);
+      counter++
+    }
+  }
+  return seatArray;
+}
+
+console.log(seatList());
