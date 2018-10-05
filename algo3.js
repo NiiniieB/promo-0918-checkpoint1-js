72e8abc69b117774215635b68bfe6e62bfd9b8e5
Patrice28300 <p.et.c.champrenault@gmail.com>
2018-10-05 11:53:51 +0200

    commit 4 , solution algo3

--- algo3.js
+++ algo3.js
@@ -1,9 +1,15 @@
-/*
-* Given a number N return the index value of the Fibonacci sequence.
-* Ex: fibo(6)  // return 13;
-* Ex: fibo(10) // return 89;
-*/
+let param = 10 ;
+fibonacci(param);
 
-function fibonacci(num) {
+function fibonacci(param) {
+  let result=[]; 
+  result.push(0);
+  result.push(1); 
   
+  for (let i = 0; i < param + 2; i++){
+    result.push(result[i] + result[i +1])
+  }
+
+  console.log(result[param+1]);
+  return result[param+1];
 }
