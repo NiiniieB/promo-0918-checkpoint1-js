2dca9df36ba09a12162410f0d797b0777a2cd7ff
Stéphane Guinot <sguinot86@gmail.com>
2018-10-05 10:04:38 +0200

    Algo solved

--- algo1.js
+++ algo1.js
@@ -17,3 +17,13 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+function siegeNumber(nbColumn, siegePerColumn){
+  let siegeNumberArray=[]
+  for(let i=1;i<=nbColumn;i++){
+    for(let j=1;j<=siegePerColumn;j++){
+      siegeNumberArray.push(i+"-"+j)
+    }
+  }
+  return siegeNumberArray;
+}
