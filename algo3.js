0843aeafb2a6e99b84e3d0705df2fdd5adb8c2d2
Agnes Ribeiro <mu21@hotmail.fr>
2018-10-05 13:04:29 +0200

    Checkpoint Algo

--- algo3.js
+++ algo3.js
@@ -1,9 +1,19 @@
 /*
-* Given a number N return the index value of the Fibonacci sequence.
-* Ex: fibo(6)  // return 13;
-* Ex: fibo(10) // return 89;
-*/
+ * Given a number N return the index value of the Fibonacci sequence.
+ * Ex: fibo(6)  // return 13;
+ * Ex: fibo(10) // return 89;
+ */
 
 function fibonacci(num) {
-  
+let nb1 = 1;
+let nb2 = 0;
+let temp;
+    while (num >= 0) {
+        temp = nb1;
+        nb1 = nb1 + nb2;
+        nb2 = temp;
+        num -= 1;
+    }
+    return nb2;
 }
+console.log(fibonacci(6));
