6e0821d1dda8d764e1d3d2db253399c0b12b8dc3
Florentin C <florentin.coyard@gmail.com>
2018-10-05 10:22:07 +0200

    Resolved Algo3, to be honest, it's mostly script found on the internet, but I did understand them

--- algo3.js
+++ algo3.js
@@ -1,9 +1,26 @@
 /*
 * Given a number N return the index value of the Fibonacci sequence.
-* Ex: fibo(6)  // return 13;
-* Ex: fibo(10) // return 89;
+* Ex: fibo(6)  // return 8;
+* Ex: fibo(10) // return 55;
 */
 
 function fibonacci(num) {
-  
+  let i = 0;
+  let j = 1;
+  let temp = 1;
+  for (let k = 0; k < num; k++) {
+    temp = i + j;
+    i = j;
+    j = temp;
+  }
+  return i;
+  // Then comes the recursive solution found on Medium, but i had to change the (num<=1) by (num<=2) or it was somehow returning the answer of fibonnaci(num+1) because I think it didn't take account of the fibonacci of 0
+  //   let factor = num - 1;
+  //   if (num <= 2) return 1;
+
+  //   return fibonacci(num - 1) + fibonacci(num - 2);
 }
+
+console.log(fibonacci(6));
+
+console.log(fibonacci(10));
