859ed24c0e899a467dcb254077a056ffaf7d41ae
Matthieu Petit <matthieuwpetit@gmail.com>
2018-10-05 14:17:37 +0200

    MàJ Algos résolutions partielles

--- algo1.js
+++ algo1.js
@@ -17,3 +17,163 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+let col_1= 0;
+for (let i= 0; i< 100; i++){
+  col_1 ++
+  console.log("1-"+col_1);
+}
+
+let col_2= 0;
+for (let i= 0; i< 100; i++){
+  col_2 ++
+  console.log("2-"+col_2);
+}
+
+let col_3= 0;
+for (let i= 0; i< 100; i++){
+  col_3 ++
+  console.log("3-"+col_3);
+}
+
+let col_4= 0;
+for (let i= 0; i< 100; i++){
+  col_4 ++
+  console.log("4-"+col_4);
+}
+
+let col_5= 0;
+for (let i= 0; i< 100; i++){
+  col_5 ++
+  console.log("5-"+col_5);
+}
+
+let col_6= 0;
+for (let i= 0; i< 100; i++){
+  col_6 ++
+  console.log("6-"+col_6);
+}
+
+let col_7= 0;
+for (let i= 0; i< 100; i++){
+  col_7 ++
+  console.log("7-"+col_7);
+}
+
+let col_8= 0;
+for (let i= 0; i< 100; i++){
+  col_8 ++
+  console.log("8-"+col_8);
+}
+
+let col_9= 0;
+for (let i= 0; i< 100; i++){
+  col_9 ++
+  console.log("9-"+col_9);
+}
+
+let col_10= 0;
+for (let i= 0; i< 100; i++){
+  col_10 ++
+  console.log("10-"+col_10);
+}
+
+let col_11= 0;
+for (let i= 0; i< 100; i++){
+  col_11 ++
+  console.log("11-"+col_11);
+}
+
+let col_12= 0;
+for (let i= 0; i< 100; i++){
+  col_12 ++
+  console.log("12-"+col_12);
+}
+
+let col_13= 0;
+for (let i= 0; i< 100; i++){
+  col_13 ++
+  console.log("13-"+col_13);
+}
+
+let col_14= 0;
+for (let i= 0; i< 100; i++){
+  col_14 ++
+  console.log("14-"+col_14);
+}
+
+let col_15= 0;
+for (let i= 0; i< 100; i++){
+  col_15 ++
+  console.log("15-"+col_15);
+}
+
+let col_16= 0;
+for (let i= 0; i< 100; i++){
+  col_16 ++
+  console.log("16-"+col_16);
+}
+
+let col_17= 0;
+for (let i= 0; i< 100; i++){
+  col_17 ++
+  console.log("17-"+col_17);
+}
+
+let col_18= 0;
+for (let i= 0; i< 100; i++){
+  col_18 ++
+  console.log("18-"+col_18);
+}
+
+let col_19= 0;
+for (let i= 0; i< 100; i++){
+  col_19 ++
+  console.log("19-"+col_19);
+}
+
+let col_20= 0;
+for (let i= 0; i< 100; i++){
+  col_20 ++
+  console.log("20-"+col_20);
+}
+
+let col_21= 0;
+for (let i= 0; i< 100; i++){
+  col_21 ++
+  console.log("21-"+col_21);
+}
+
+let col_22= 0;
+for (let i= 0; i< 100; i++){
+  col_22 ++
+  console.log("22-"+col_22);
+}
+
+let col_23= 0;
+for (let i= 0; i< 100; i++){
+col_23 ++
+console.log("23-"+col_23);
+}
+
+let col_24= 0;
+for (let i= 0; i< 100; i++){
+col_24 ++
+console.log("24-"+col_24);
+}
+
+let col_25= 0;
+for (let i= 0; i< 100; i++){
+col_25 ++
+console.log("25-"+col_25);
+}
+
+let col_26= 0;
+for (let i= 0; i< 100; i++){
+col_26 ++
+console.log("26-"+col_26);
+}
+
+
+
+
