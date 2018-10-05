8d0c8695c58ece4066ffd1dc1df887856129a5c4
Patrice28300 <p.et.c.champrenault@gmail.com>
2018-10-05 10:46:18 +0200

    commit 2 , solution algo1 et algo2

--- algo1.js
+++ algo1.js
@@ -1,19 +1,17 @@
-/*
-Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
-Dans la salle les sièges sont répartis de la manière suivantes : 
-  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
-  - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
+listeSieges();
 
-Au final la liste devra ressembler à : 
-1-1 
-1-2
-1-3
-.
-.
-.
-26-98
-26-99
-26-100
+function listeSieges(){
+  let resultat=[];
 
-Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
-*/
+  for (let j = 0 ; j<=25 ;j++){
+    for (let i = 1 ; i<=100 ;i++){
+        resultat[i+j*100]= (j+1)+"-"+i;
+    }
+  }
+  
+  console.log(resultat);
+
+  return resultat;
+
+}
+
--- algo2.js
+++ algo2.js
@@ -1,34 +1,22 @@
-/* 
-* Corriger la fonction minMax()
-* La fonction récupère un tableau de nombres positifs, négatifs ou nuls
-* Et retourne un tableau à 2 entrées contenant la valeur minimale
-* et la valeur maximale du tableau récupéré en entrée
-* 
-* Exemple 1 :
-* Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
-* Résultat : [-65, 67]
-* 
-* Exemple 2 :
-* Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
-* Résultat : [-30, 108]
-* 
-* Exemple 3 :
-* Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
-* Résultat : [7, 85]
-*/
+let tab=[2,-5,3,-6,100,4];
+minMax(tab);
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+function minMax(arr){
+   let min=arr[0];
+   let max=arr[0];
+   let result=[];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
-    }
-    if (array[i] = max) {
-      max = array[i];
-    }
-    return array;
-  }
-  array.push(min, max);
+   for (let i = 0; i < arr.length; i++) {
+     if (arr[i] < min) { 
+        min=arr[i];       
+     }
+     if (arr[i] > max) { 
+        max=arr[i];       
+     }
+   }
+   result.push(min,max);
+
+   console.log(result);
+   return result;
 }
+
