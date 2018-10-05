496521959257e46ce7042ad2ae2a8cc3d6a5136b
RmTrnsc <tournesacromain@gmail.com>
2018-10-05 12:55:01 +0200

    solution non résolu algo1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,22 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+function myArray(){
+
+let nbColonnes = 0;
+let nbSieges = 0;
+let siegeC = 0;
+let siegeL = 0;
+let siege = [];
+
+  for (i = 0; i < nbColonnes.length; i ++){ 
+    siegeC = nbColonnes[i];
+    for ( i = 0; i < nbSieges.length; i ++){ 
+      siegeL = nbSieges[i];
+    }
+    siege.push(siegeC);
+    siege.push(siegeL);
+  }
+  return siege;
+}
+console.log(myArray([1, 2, 4, 5]))
