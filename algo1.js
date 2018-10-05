d52da17dfc06c93586d303bd068d0308662cd303
Audrey LOURI <audrey.louri@gmail.com>
2018-10-05 12:46:44 +0200

    solution algo1.js

--- algo1.js
+++ algo1.js
@@ -17,3 +17,15 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const sieges = () => { 
+  const tab = []
+  for (let i = 1; i < 27; i++) {
+    for (let j = 1; j < 101; j++) {
+      tab.push(i+"-"+j)    
+    }
+  }
+  return tab
+}
+const stock = sieges()
+console.log(stock)
