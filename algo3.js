a9eaf82d65696690369a0996f6f29b5489f393ed
Hugo Hontans <hugohontans@gmail.com>
2018-10-05 12:26:58 +0200

    j'ai fais l'algo3 bonus

--- algo3.js
+++ algo3.js
@@ -5,5 +5,17 @@
 */
 
 function fibonacci(num) {
-  
+	let suiteNum = [1, 2];
+	if(num == 1) {return suiteNum[0]}
+	else if (num == 2) {return suiteNum[1]}
+	else {
+	  	for (let i=2; i < num; i ++){
+	  	suiteNum.push(suiteNum[i-2] + suiteNum[i-1]);
+	  	}
+	  
+	}
+	return suiteNum[num-1];
 }
+
+console.log(fibonacci(6));
+console.log(fibonacci(10));
