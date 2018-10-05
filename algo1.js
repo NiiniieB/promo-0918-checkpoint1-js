e4077a07527bae56afeaf0822e1af7c52edf0967
Julien sch <julien.schader@laposte.net>
2018-10-05 10:53:19 +0200

    result algo1.js

--- algo1.js
+++ algo1.js
@@ -17,3 +17,19 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+
+function tableau() {
+  let nbSiege = [];
+  for (let i = 1; i < 27; i++) {
+    for (let j = 1; j < 101; j++) {
+      nbSiege.push(i + "-" + j);
+    }
+  }console.log(nbSiege);
+}
+
+return tableau();
+
+
+
