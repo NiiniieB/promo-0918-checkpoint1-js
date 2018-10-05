569d435a31281cd4715224fa7e796dd55e7b8d93
JulesGrenier <grenier.jules@outlook.com>
2018-10-05 10:57:14 +0200

    Algo 3 terminé

--- algo3.js
+++ algo3.js
@@ -5,5 +5,14 @@
 */
 
 function fibonacci(num) {
-  
+    let fibo = [0, 1];
+    let firstLast, secondLast;
+
+    for(let i = 0; i < num; i++){
+        firstLast = fibo.length-1;
+        secondLast = fibo.length-2;
+        fibo.push(fibo[firstLast]+fibo[secondLast]);
+    }
+    
+    return fibo[fibo.length-1];
 }
