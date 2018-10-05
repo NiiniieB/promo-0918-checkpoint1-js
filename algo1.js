7f48b0bbd14a4eb0f510db72e392009586630d74
lydia.dal@live.fr <lydia.dal@live.fr>
2018-10-05 11:30:18 +0200

    algo1

--- algo1.js
+++ algo1.js
@@ -17,3 +17,17 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+function siege(array){
+  var grille = new Array();
+  
+  for(var i=1; i<100; i++)
+     grille[i] = new Array();
+  
+  for(var i=1; i<27; i++)
+     for(var j=1; j<101; j++)
+        console.log(i + "-" + j);
+  
+  }
+  
+  siege()
