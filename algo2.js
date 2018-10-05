79a8a8a9f4cfd6d0bb64dd9b7811c4d3868e1c1d
Axielles <awacks@gmail.com>
2018-10-05 11:15:38 +0200

    algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,21 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+let minMax = (array) => {
+  let miniMaxi = []
+  let min = 1000000;
+  let max = -1000000;
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
-    }
-    if (array[i] = max) {
-      max = array[i];
-    }
-    return array;
+  for (i = 0; i < array.length; i++) {
+      if (array[i] < min)
+          min = array[i];
+      else if (array[i] > max)
+          max = array[i];
   }
-  array.push(min, max);
+  miniMaxi.push(min, max);
+  return miniMaxi;
 }
+
+console.log(minMax ([4, 6, 35, -65, -9, 0, 67]))
+console.log(minMax ([-30, 5, 43, 108, -5, -7, 89]))
+console.log(minMax ([56, 7, 63, 9, 7, 12, 85]))
