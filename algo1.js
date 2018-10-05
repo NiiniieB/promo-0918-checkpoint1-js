61a1930c2297b4d2a3b811f8c6f6ba47a9c73156
Johann Teyssandier <johann.teyssandier@protonmail.com>
2018-10-05 11:24:20 +0200

    algo1

--- algo1.js
+++ algo1.js
@@ -1,11 +1,23 @@
+
+for (i=1; i<27; i++){
+  let colonnes=[i];
+    for (y=1; y<101; y++){
+      let places=[y]
+      let result = colonnes + "-" + places
+      console.log(result)
+
+      }
+}
+
+
 /*
 Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
-Dans la salle les sièges sont répartis de la manière suivantes : 
+Dans la salle les sièges sont répartis de la manière suivantes :
   - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
   - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
 
-Au final la liste devra ressembler à : 
-1-1 
+Au final la liste devra ressembler à :
+1-1
 1-2
 1-3
 .
