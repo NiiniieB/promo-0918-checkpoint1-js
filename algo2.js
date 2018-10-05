efc5b1b9c37e37ab82862fbce341da544a56a29c
Mérédith Mougeot <meredith75017@hotmail.fr>
2018-10-05 18:14:57 +0200

    ajout algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+var minMax = (array) => {
+  let min = array[0];
+  let max = array[0];
+  const array2 = []
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+  for (let i = 0; i < array.length ; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    if (array[i] = max) {
+    if (array[i] > max) {
       max = array[i];
     }
-    return array;
   }
-  array.push(min, max);
+  array2.push(min,max);
+  return array2
 }
+console.log(minMax([36, 0, 6, -4785, 45, 966, 876]))
