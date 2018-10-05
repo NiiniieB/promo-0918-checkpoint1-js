c7bf1b10eb65add7546cd91b539f1f958020bd88
Elisa Bourgeois <elisabourgeois.eb@gmail.com>
2018-10-05 10:46:08 +0200

    Resolution algo3.js

--- algo3.js
+++ algo3.js
@@ -5,5 +5,22 @@
 */
 
 function fibonacci(num) {
-  
+    //call a recursive function initialized with the 2 first numbers of the Fibonacci sequence (1, 1) and the index of the value to get
+    return rFibo(1, 1, num);
 }
+
+const rFibo = (iA, iB, iN) => {
+    if (iN > 1) {
+        /*At each run :
+            iA -> iB, 
+            iB -> iA + iB,
+            iN -> iN - 1
+        */
+        return rFibo(iB, iA+iB, iN-1);
+    }
+    //if N > 1 then return the value of iB
+    return iB;
+}
+
+console.log(fibonacci(6));
+console.log(fibonacci(10));
