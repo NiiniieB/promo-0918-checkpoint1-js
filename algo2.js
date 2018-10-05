5b92d21736839d598869602e5ade2e8828631cc2
Electric_Sheep <iamthebou@gmail.com>
2018-10-05 10:32:32 +0200

    Algo2.js completed

--- algo2.js
+++ algo2.js
@@ -17,18 +17,12 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+const myArrayToCheck = [56, 7, 63, 9, 7, 12, -85];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
-    }
-    if (array[i] = max) {
-      max = array[i];
-    }
-    return array;
-  }
-  array.push(min, max);
-}
+const minMax = toCheck => {
+  let result = [];
+  result.push(Math.min(...toCheck), Math.max(...toCheck));
+  console.log(result);
+};
+
+minMax(myArrayToCheck);
