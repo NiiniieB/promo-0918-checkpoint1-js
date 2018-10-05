3ea2e11e064607b43858945f44a5e9685e47c41c
Aurélien <aureliano14@hotmail.fr>
2018-10-05 12:21:40 +0200

    Commit de l'exo algo3 ainsi que de l'index.html et .css de la partie 3

--- algo3.js
+++ algo3.js
@@ -5,5 +5,15 @@
 */
 
 function fibonacci(num) {
-  
+    let tab=new Array(0,1);
+    for (let i=2; i<=num+1; i++) {
+        tab[i]=tab[i-1]+tab[i-2];
+    }
+   return tab[num+1];
 }
+
+let ret6 = fibonacci(6);
+let ret10 = fibonacci(10);
+
+console.log("La 6ème valeur de la suite de Fibonacci est : " + ret6);
+console.log("La 10ème valeur de la suite de Fibonacci est : " + ret10);
