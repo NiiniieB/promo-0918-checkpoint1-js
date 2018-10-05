1c529cffd3c745197e1d4d308beac7d6f778f608
AnaMilten <anaismiltenberger@gmail.com>
2018-10-05 12:07:51 +0200

    ajout exercice algo 3

--- algo3.js
+++ algo3.js
@@ -3,7 +3,17 @@
 * Ex: fibo(6)  // return 13;
 * Ex: fibo(10) // return 89;
 */
-
 function fibonacci(num) {
-  
+    const array = [];
+    let start = 1;
+    let count = 0;
+
+    for (let i = 0; i <= num; i++) {
+        let current = count + start;
+        array.push(current);
+        start = count;
+        count = current;
+    }
+
+    return array[num];
 }
