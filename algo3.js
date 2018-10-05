90799244b10f5bc0175736a6467d3e4a544f3cac
Alex <alex.gauthier41@gmail.com>
2018-10-05 10:58:03 +0200

    Resolution de l'algo3

--- algo3.js
+++ algo3.js
@@ -4,6 +4,16 @@
 * Ex: fibo(10) // return 89;
 */
 
-function fibonacci(num) {
-  
+function fibonacci(num) {   
+    let arr = [0, 1];
+    for (let i = 2; i < num + 1; i++){
+      arr.push(arr[i - 2] + arr[i -1])
+    }
+   return arr[num]
 }
+
+// Ex: fibo(6) *7* in JS  // return 13;
+console.log(fibonacci(7))
+
+// Ex: fibo(10) *11* in JS // return 89;
+console.log(fibonacci(11))
