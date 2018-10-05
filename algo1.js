be367c08f995b185217b6be23b5066871ff594cd
Reb Sen <archivesreb@gmail.com>
2018-10-05 13:10:40 +0200

    13h sending bootstrap-ok and alogo not ok

--- algo1.js
+++ algo1.js
@@ -17,3 +17,40 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+let column = 0
+let nbPlace = 0
+
+let column = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+
+function CountSeats(room) {
+  return room.map(function(col) {
+   let seat = 1;
+   for (let i=0, i<=100, i++) {
+    seat +=1
+   
+    return '${col}-${seat}
+    
+  });
+}
+
+
+
+
+let nbPlace = '';
+    while ()
+      for (col=0, col <=26, i++) {
+        
+      }
+let column = 1;
+let seat = 1;
+for (let i=0, i<=100, i++) {
+  seat +=1
+
+        
+          
+        return '${column}-${seat}'};
+
+
+
--- algo2.js
+++ algo2.js
@@ -17,18 +17,39 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+// var minMax = () => (array) => {
+//   const min = 0;
+//   const max = array[0];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+//   for (let i = 1; i = array.length - 1; i+1) {
+//     if (array(i) < min) {
+//       min = array;
+//     }
+//     if (array[i] = max) {
+//       max = array[i];
+//     }
+//     return array;
+   
+//   }
+//   array.push(min, max);
+// }
+
+function minMax(array) 
+
+let minMax = () => (array) => {
+    const min = 0;
+    const max = array[0];
+  
+    for (let i = 1; i = array.length - 1; i+1) {
+      if (array(i) < min) {
+        min = array;
+      }
+      if (array[i] = max) {
+        max = array[i];
+      }
+      return array;
+     
     }
-    if (array[i] = max) {
-      max = array[i];
-    }
-    return array;
+    array.push(min, max);
   }
-  array.push(min, max);
-}
+
