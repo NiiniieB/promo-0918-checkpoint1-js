89a0e9a309fe38459eb23cec5aa7b0fcf70a0fe2
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-10-05 10:13:05 +0200

    algo3.js finished

--- algo3.js
+++ algo3.js
@@ -3,7 +3,10 @@
 * Ex: fibo(6)  // return 13;
 * Ex: fibo(10) // return 89;
 */
+const assert = require("assert")
 
-function fibonacci(num) {
-  
-}
+const fibonacci = num => num <= 1 ? 1 : fibonacci(num - 1) + fibonacci(num - 2);
+
+
+assert.strictEqual(fibonacci(6), 13)
+assert.strictEqual(fibonacci(10), 89)
