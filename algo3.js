0e5038f28f2e77d2f3344a933571e03a5b8e8c91
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2018-10-06 11:56:21 +0200

    solved algo3.js + pseudo-code

--- algo3.js
+++ algo3.js
@@ -17,3 +17,12 @@ function fibonacci(num) {
 
 
 console.log(fibonacci(6));
+
+
+//créé une fonction "fibonacci" qui prend 1 nb  "num" pour parametre
+//créé une constante tableau "suite" qui a pour valeurs 1 et 1
+// repète tant que la valeur est inf à 1000 :
+//      - calcule la valeur suivante du tableau en additionnant la valeur actuelle à la valeur précédente,
+//      - puis envoie cette valeur à la fin du tableau
+// créé une variable " nbToFind" qui définit l'index du tableau en fonction du nb "num" donné
+// retourne la valeur du tableau "suite" correspondant à l'index "num"
