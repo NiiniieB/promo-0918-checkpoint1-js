07a2ec041d604d841dffd550b60e7d2b88363290
Tiphaine Deswarte <tiphaine.deswarte@live.fr>
2018-10-06 18:31:28 +0200

    nouvelle solution

--- algo1.js
+++ algo1.js
@@ -17,17 +17,54 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
-let number= 1;
+
+/*
+let nb= [];
 let array= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 let row = (array[1]);
 function Seats(nb){
-  for (let i=0; i<26 ; i++) { 
-    return array += number
+  for(let i=0; i < 26; i++) { 
+    return array += nb;
 
     
    }
    
     
 }
-console.log (array);
+console.log (nb);
+*/
 
+/* afficher 100 fois le chiffre 1, le chiffre 2 etc jusqu'à 26
+// et ajouter +1 à chaque ligne pour avoir 1-2, 1-3 ...
+function array(){
+  let nbSeats = [];
+  for(let i = 1; i < 27; i++){
+    for(let j = 1; j<101; j++){
+      nbSeats.push(i+"-" +j);
+    }
+  }console.log(nbSeats); 
+
+}
+
+return array();
+
+*/
+
+
+function array(){
+  let nbSeats = [];
+  for(let i = 1; i < 27; i++){
+    for(let j = 1; j<101; j++){
+      nbSeats.push(i+"-" +j);
+    }
+  }console.log(nbSeats); 
+
+}
+
+return array();
+
+
+
+  
+
+  
