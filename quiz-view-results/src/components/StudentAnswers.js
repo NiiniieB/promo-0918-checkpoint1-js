a22aa3b2a17b40881f984508fe99ca4e5660d10c
Benoît Hubert <benoithubert@gmail.com>
2018-10-08 01:40:49 +0200

    Dernier fix sur StudentAnswers

--- quiz-view-results/src/components/StudentAnswers.js
+++ quiz-view-results/src/components/StudentAnswers.js
@@ -9,11 +9,17 @@ const statusClassMap = {
 
 const StudentAnswers = ({student}) => (
   <div style={{ height: '400px', overflowY: 'scroll' }}>
+    <h6>Légende</h6>
+    <ul>
+      <li className="text-success">réponse correcte</li>
+      <li className="text-info">réponse correcte manquante</li>
+      <li className="text-danger">réponse incorrecte</li>
+    </ul>
   {
     student.tests.map(({ questionText, statuses }, qIdx) => (
       <div key={qIdx}>
         <h6>Q{qIdx + 1}. {questionText}</h6>
-        <ol>
+        <ol style={{ listStyleType: 'lower-latin' }}>
           {
             statuses.map(({ nom, status }, ansIdx) => (
               <li key={ansIdx} className={statusClassMap[status]}>{nom}</li>
