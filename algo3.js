62d989ccb490c5c7c0db234308505ca9921b87ac
Julien <julien.bertocchini@gmail.com>
2018-10-07 11:29:36 +0200

    correction algo3

--- algo3.js
+++ algo3.js
@@ -1,9 +1,22 @@
 /*
+Je pense qu'il y a une erreur sur les return dans l'énoncé.
+
 * Given a number N return the index value of the Fibonacci sequence.
-* Ex: fibo(6)  // return 13;
+* Ex: fibo(6)  // return 13; 
 * Ex: fibo(10) // return 89;
 */
 
+
+
+let num = 6
+
 function fibonacci(num) {
-  
+let array = [0, 1];
+
+    for (let i = 2; i < num + 1; i++){
+        array.push(array[i - 2] + array[i - 1]);
+    }
+return array[6];
 }
+console.log(fibonacci(num)) 
+
