f2c5547a4cfa3c523c9a3b2f95c82c5ece7048d1
Reb Sen <archivesreb@gmail.com>
2018-10-08 23:35:25 +0200

    algo3 ok - histoire de - pour une branche plus clean

--- algo3.js
+++ algo3.js
@@ -5,5 +5,13 @@
 */
 
 function fibonacci(num) {
-  
+let fibon = [1, 1]
+let i = 0;
+while(i <3000) {
+    fibon.push(fibon[i] + fibon[i+1]);
+    i++;
 }
+  return fibon[num]
+}
+
+console.log(fibonacci(300));
