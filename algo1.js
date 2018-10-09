6608336dafab34e20d7d9b96b041a826facd9c94
Benoit Niveau <be.niveau@hotmail.fr>
2018-10-09 11:12:42 +0200

    algo1_new_version

--- algo1.js
+++ algo1.js
@@ -17,12 +17,17 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
-
-const array = [];
-
-for (let i = 1; i <= 26; i++) {
-  for (let j = 1; j <= 100; j++) {
-    array.push(i + "-" + j);
+function cinema(i,j) {
+  const array=[]
+    for (let i = 1; i <= 26; i++) {
+      for (let j = 1; j <= 100; j++) {
+        array.push(i + "-" + j);
+      }
+    }
+    return array;
+    
   }
-}
-console.log(array);
+  
+  */test*/
+  cinema(26,100)
+  
