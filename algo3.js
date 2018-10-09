a711a69dc5a265965d6f2cd566c48ee327c35701
Julien sch <julien.schader@laposte.net>
2018-10-09 15:35:37 +0200

    correction algo3

--- algo3.js
+++ algo3.js
@@ -5,5 +5,29 @@
 */
 
 function fibonacci(num) {
-  
+  let fibo = [];
+  fibo[0] = 1;
+  fibo[1] = 1;
+
+  for (let i = 2; i <= num; ++i) {
+    fibo[i] = fibo[i-1] + fibo [i-2];
+  }
+  return fibo[num];
 }
+
+console.log(fibonacci(6));
+
+
+/* fonction récursive */
+
+function fibonacciRecurcive(num) {
+    if (num == 0) {
+        return 1;
+    }else if (num == 1) {
+        return 1;
+    }else {
+        return fibonacciRecurcive(num-1) + fibonacciRecurcive(num-2);
+    }
+}
+
+console.log(fibonacciRecurcive(6));
