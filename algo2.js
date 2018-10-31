72754788803ed305b2d6f0154cc34985eea1cd7e
Anja Razafitrimo <anjazarafitrimo@msn.com>
2018-10-31 11:55:49 +0100

    algo2 fini

--- algo2.js
+++ algo2.js
@@ -17,18 +17,24 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+/* J'ai laissé l'exemple 1 dans le tableau d'entrée pour vérifier que ça fonctionne bien*/
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+let array = [4, 6, 35, -65, -9, 0, 67];
+let min = array[0];
+let max = array[0];
+let array2 =[];
+
+
+var minMax = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    if (array[i] = max) {
+    if (array[i] > max) {
       max = array[i];
     }
-    return array;
   }
-  array.push(min, max);
+  array2.push(min, max);
+  return array2;
 }
+console.log(minMax(array))
