96b97472cccc3fb6ccca593f31494959392ff555
Thomas Lambert <lambertthomas10@gmail.com>
2018-10-31 10:51:18 +0100

    Fin de algo2.js

--- algo2.js
+++ algo2.js
@@ -17,18 +17,20 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
-
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+const minMax = array => {
+  let min = array[1];
+  let max = array[1];
+  let lastArray = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    if (array[i] = max) {
+    else if (array[i] > max) {
       max = array[i];
     }
-    return array;
   }
-  array.push(min, max);
+  lastArray.push(min, max);
+  return lastArray;
 }
+
+minMax([25,444,-11,4]);
