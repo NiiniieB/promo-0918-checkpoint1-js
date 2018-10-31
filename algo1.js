ce908aed25dde56f901feb973ae012a83f54b196
Audrey Mertens <audrey.mertens@hotmail.be>
2018-10-31 12:03:56 +0100

    j'ai commencé les 2 exos mais ai pas fini, horrible

--- algo1.js
+++ algo1.js
@@ -1,11 +1,11 @@
 /*
 Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
-Dans la salle les sièges sont répartis de la manière suivantes : 
+Dans la salle les sièges sont répartis de la manière suivantes :
   - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
   - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
 
-Au final la liste devra ressembler à : 
-1-1 
+Au final la liste devra ressembler à :
+1-1
 1-2
 1-3
 .
@@ -17,3 +17,19 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+const colonnes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
+const sieges = [1 .. 100]
+
+
+function SiegesTheatre(array){
+  let listeSieges = [];
+  for (let i = 0; i < array.length; i++) {
+    listeSieges.push(array[i]);
+    for (let j = 0; j < sieges.length; j++) {
+      listeSiege = listeSiege[0] + "-" + array[j]
+    }
+  }
+}
+SiegesTheatre(colonnes);
+
--- algo2.js
+++ algo2.js
@@ -1,17 +1,17 @@
-/* 
+/*
 * Corriger la fonction minMax()
 * La fonction récupère un tableau de nombres positifs, négatifs ou nuls
 * Et retourne un tableau à 2 entrées contenant la valeur minimale
 * et la valeur maximale du tableau récupéré en entrée
-* 
+*
 * Exemple 1 :
 * Tableau d'entrée : [4, 6, 35, -65, -9, 0, 67]
 * Résultat : [-65, 67]
-* 
+*
 * Exemple 2 :
 * Tableau d'entrée : [-30, 5, 43, 108, -5, -7, 89]
 * Résultat : [-30, 108]
-* 
+*
 * Exemple 3 :
 * Tableau d'entrée : [56, 7, 63, 9, 7, 12, 85]
 * Résultat : [7, 85]
@@ -21,8 +21,8 @@ var minMax = () => (array) => {
   const min = 0;
   const max = array[0];
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
       min = array;
     }
     if (array[i] = max) {
@@ -32,3 +32,26 @@ var minMax = () => (array) => {
   }
   array.push(min, max);
 }
+
+
+
+
+let nombre = [4, 6, 35, -65, -9, 0, 67]
+
+function extreme (array){
+  for ( let i = 0; i < array.lenght, i++){
+    let stock = 0;
+    for ( let j = 0; j < array.lenght, j++){
+      if array[j] < array[i]{
+        stock = array[j];
+        array[j] = array[i];
+        array[i] = stock;
+      }
+    }
+
+  }
+  return array;
+}
+
+extreme(nombre)
+
