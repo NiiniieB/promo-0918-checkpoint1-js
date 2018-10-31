734160b26eaa164289f88c59818fae40758140a6
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-10-31 12:00:37 +0100

    fibionaci

--- algo3.js
+++ algo3.js
@@ -3,7 +3,26 @@
 * Ex: fibo(6)  // return 13;
 * Ex: fibo(10) // return 89;
 */
-
+let i = 0;
+let numF= 0;
+let numPre= 0;
+let numDeu= 0
 function fibonacci(num) {
-  
+  if(num ===0){
+      return 0;
+  }  
+  if(i === num)
+  {
+      return numF;
+  }
+  if(i === 0){
+    numPre = 1
+  }
+  if(i < num){
+      numF = numPre + numDeu;
+      numDeu = numPre;
+      numPre = numF;
+      i++;
+      fibonacci(num);
+  }
 }
