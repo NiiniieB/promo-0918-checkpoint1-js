2d3474fa146c7b01b9598cd0fe967b4405ae12f0
cormobro <bonaet.felix@gmail.com>
2018-10-31 10:26:02 +0100

    ajotu exercice aglo3.js

--- algo3.js
+++ algo3.js
@@ -5,5 +5,14 @@
 */
 
 function fibonacci(num) {
-  
-}
+	if(num < 0){
+		return(-1);
+	}
+	else if(num === 0){
+		return(0);
+	}
+	else if(num === 1){
+		return(1);
+	}
+	return(fibonacci(num - 1) + fibonacci(num - 2));
+	}
