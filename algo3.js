884421d920c1f3f83aa4bc75ed4afe9f9dccd3c3
Thomas Lambert <lambertthomas10@gmail.com>
2018-10-31 11:43:54 +0100

    Fin algo3

--- algo3.js
+++ algo3.js
@@ -5,5 +5,12 @@
 */
 
 function fibonacci(num) {
-  
+    let follow = [1,1];
+    let nextFollow =""
+    for(i = 0; i < num; i++){
+        nextFollow = follow[i+1] + follow[i];
+        follow.push(nextFollow)
+    }
+    return follow[num];
 }
+fibonacci(10);
