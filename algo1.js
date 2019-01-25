be31a0116863ae0a2ff383745fe53f361b9e06f8
NiiniieB <mel.betourne.14@gmail.com>
2019-01-25 15:15:38 +0100

    Algo1

--- algo1.js
+++ algo1.js
@@ -1,7 +1,7 @@
 /*
 Un employé de théatre voudrait afficher la liste de tous les sièges présents dans sa salle principale.
 Dans la salle les sièges sont répartis de la manière suivantes : 
-  - Il y a 26 colonnes de sièges, numérotés de "1" à "26"
+  - Il y a 26 colonne de sièges, numérotés de "1" à "26"
   - Chaque colonne contient 100 sièges, numérotés de "1" à "100"
 
 Au final la liste devra ressembler à : 
@@ -17,3 +17,20 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+var chair={
+        column : 26,
+        row : 100
+      }
+var table=new Array;
+      function theater(chair) {
+        for(let i = 1; i <= chair.column; i++){
+          for(let y = 1; y <= chair.row; y++){
+                table.push((i)+ "-"+(y))
+          }
+        }
+        return table;
+      }
+theater(chair)
+console.log(table);
